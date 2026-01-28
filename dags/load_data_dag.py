684dcd5ce193d972f5526dcc9c6af8d2f0044cf5
Karin <karinversan@gmail.com>
2026-01-28 07:04:29 +0300

    infra: load data

---
+++ dags/load_data_dag.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+from src.ml.load_features import load_csv_to_postgres
+
+with DAG(
+    dag_id="load_data_to_postgres",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+    load_task = PythonOperator(
+        task_id="load_csv",
+        python_callable=load_csv_to_postgres
+    )
---
+++ src/ml/load_features.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+
+DB_URI = "postgresql+psycopg2://mlops:mlops@postgres:5432/segmentation"
+
+
+RENAME = {
+    "CUST_ID": "customer_id",
+    "BALANCE": "balance",
+    "BALANCE_FREQUENCY": "balance_frequency",
+    "PURCHASES": "purchases",
+    "ONEOFF_PURCHASES": "oneoff_purchases",
+    "INSTALLMENTS_PURCHASES": "installment_purchases",
+    "CASH_ADVANCE": "cash_advance",
+    "PURCHASES_FREQUENCY": "purchases_frequency",
+    "ONEOFF_PURCHASES_FREQUENCY": "oneoff_purchases_frequency",
+    "PURCHASES_INSTALLMENTS_FREQUENCY": "purchases_installments_frequency",
+    "CASH_ADVANCE_FREQUENCY": "cash_advance_frequency",
+    "CASH_ADVANCE_TRX": "cash_advance_trx",
+    "PURCHASES_TRX": "purchases_trx",
+    "CREDIT_LIMIT": "credit_limit",
+    "PAYMENTS": "payments",
+    "MINIMUM_PAYMENTS": "minimum_payments",
+    "PRC_FULL_PAYMENT": "prc_full_payment",
+    "TENURE": "tenure",
+}
+
+def load_csv_to_postgres(csv_path: str = "/opt/airflow/data/Customer Data.csv"):
+    df = pd.read_csv(csv_path)
+
+    df = df.rename(columns=RENAME)
+
+
+    df["event_timestamp"] = pd.Timestamp.utcnow()
+
+
+    cols = ["customer_id", "event_timestamp"] + [v for v in RENAME.values() if v != "customer_id"]
+    df = df[cols]
+
+    engine = create_engine(DB_URI)
+
+    with engine.begin() as conn:
+        conn.execute(text("TRUNCATE TABLE customer_features;"))
+    df.to_sql("customer_features", engine, if_exists="append", index=False, chunksize=2000)
